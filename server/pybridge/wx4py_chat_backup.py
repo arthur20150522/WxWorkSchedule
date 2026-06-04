7a6cbabfbe40e8e1c462b94ea41aa898540a8457
Alex <chenpeihong@4399inc.com>
2026-06-04 20:15:46 +0800

    fix-1: disable all wx4py _minimize_window() calls in chat.py

---
+++ server/pybridge/wx4py_chat_backup.py
@@ -0,0 +1,5 @@
+** WARNING: connection is not using a post-quantum key exchange algorithm.
+** This session may be vulnerable to "store now, decrypt later" attacks.
+** The server may need to be upgraded. See https://openssh.com/pq.html
+'cat' 不是内部或外部命令，也不是可运行的程序
+或批处理文件。
